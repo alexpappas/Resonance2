19dc28a71ed01969ba30324f74ae97e52cb8c101
alexpappas <abp400@nyu.edu>
2017-04-04 14:50:27 -0400

    Merged in Scoring
    Ruowen’s Scoring is added… hope to fix the YAML too…

--- Assets/Resources/Scripts/ResonatorScript.cs
+++ Assets/Resources/Scripts/ResonatorScript.cs
@@ -81,9 +81,9 @@ public class ResonatorScript : MonoBehaviour {
 		//	and if that pentagon is rotating it turns off
 		//	if people like it this week this is our next big project
 		//	that i'm really gonna need you're help with!
-		if (this.GetComponentInParent<PappasBigRotate> () == null) {
+		if (this.GetComponentInParent<BigRotation> () == null) {
 			//print ("AH");
-		} else if (this.GetComponentInParent<PappasBigRotate> ().isRotating) {
+		} else if (this.GetComponentInParent<BigRotation> ().isRotating) {
 			isResonating = false;
 			SetMyColor ();
 			CancelInvoke ("ExecutePulse");
