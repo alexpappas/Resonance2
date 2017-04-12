a5f6bbc1dc1758d6a8c1973e96432c6efe39679f
alexpappas <abp400@nyu.edu>
2017-04-12 12:00:26 -0400

    Visuals
    player pulse is now different
    temporary fix for wall image (will explain in person… for now roll with
    it)

---
+++ Assets/Resources/Scripts/PulseColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulseColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (name == "Player's Pulse") {
+			this.GetComponent<TrailRenderer> ().startColor = Color.cyan;
+			this.GetComponent<TrailRenderer> ().endColor = Color.white;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
