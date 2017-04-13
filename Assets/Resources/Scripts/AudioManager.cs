5f4cbb939680181df1677585479a81f0c22a66fa
alexpappas <abp400@nyu.edu>
2017-04-12 21:59:00 -0400

    AudioManager2.0
    sound effects are all run locally in Unity
    ambient background music has been removed for alex to reevaluate

--- Assets/Resources/Scripts/AudioManager.cs
+++ Assets/Resources/Scripts/AudioManager.cs
@@ -8,6 +8,25 @@ public class AudioManager : MonoBehaviour {
 
 	public bool audioON;
 
+	AudioClip[] triangleSounds;
+	AudioClip[] squareSounds;
+	AudioClip[] pentagonSounds;
+
+	public AudioSource[] resonatorPolyphony;
+	int resonatorVoiceCounter;
+
+	AudioClip[] triangleRotationSounds;
+	AudioClip[] squareRotationSounds;
+	AudioClip[] pentagonRotationSounds;
+
+	public AudioSource[] rotationPolyphony;
+	int rotationVoiceCounter;
+
+	public AudioSource playerVoice;
+	int currentClip;
+
+	public AudioSource goalVoice;
+
 	void Awake () {
 		//	create singleton instance of AudioManager
 		if (instance == null) {
@@ -18,29 +37,82 @@ public class AudioManager : MonoBehaviour {
 
 		if (audioON) {
 			//	for communication with MaxMSP
-			OSCHandler.Instance.Init ();
+		//	OSCHandler.Instance.Init ();
 
-			OSCHandler.Instance.SendMessageToClient ("MAX", "127.0.0.1", "onoff 1");
+		//	OSCHandler.Instance.SendMessageToClient ("MAX", "127.0.0.1", "onoff 1");
+		
+			InitializeSounds ();
 		}
 	}
-
+		
 	//	sends player pulse information to Max
 	public void PlayerPulse(int x) {
 		if (audioON) {
-			OSCHandler.Instance.SendMessageToClient ("MAX", "127.0.0.1", "pulse " + x);
+			//OSCHandler.Instance.SendMessageToClient ("MAX", "127.0.0.1", "pulse " + x);
+
+			if (x == 3) {
+				currentClip = 1 + Random.Range (0, (triangleSounds.Length - 1));
+				playerVoice.clip = triangleSounds [currentClip];
+			} else if (x == 4) {
+				currentClip = 1 + Random.Range (0, (squareSounds.Length - 1));
+				playerVoice.clip = squareSounds [currentClip];			
+			} else if (x == 5) {
+				currentClip = 1 + Random.Range (0, (pentagonSounds.Length - 1));
+				print (currentClip);
+				playerVoice.clip = pentagonSounds [currentClip];
+			}
+
+			playerVoice.Play();
+
 		}
 	} 
 
 	//	sends resonator pulse information to Max
 	public void ResonatorPulse(int x) {
 		if (audioON) {
-			OSCHandler.Instance.SendMessageToClient ("MAX", "127.0.0.1", "pulse " + x);
+			//OSCHandler.Instance.SendMessageToClient ("MAX", "127.0.0.1", "pulse " + x);
+
+			if (x == 3) {
+				currentClip = Random.Range (0, (triangleSounds.Length - 1));
+				resonatorPolyphony[resonatorVoiceCounter].clip = triangleSounds [currentClip];
+				resonatorPolyphony [resonatorVoiceCounter].Play ();
+			} else if (x == 4) {
+				currentClip = Random.Range (0, (squareSounds.Length - 1));
+				resonatorPolyphony[resonatorVoiceCounter].clip = squareSounds [currentClip];
+				resonatorPolyphony [resonatorVoiceCounter].Play ();			
+			} else if (x == 5) {
+				currentClip = Random.Range (0, (pentagonSounds.Length - 1));
+				resonatorPolyphony[resonatorVoiceCounter].clip = pentagonSounds [currentClip];
+				resonatorPolyphony [resonatorVoiceCounter].Play ();
+			}
+
+			resonatorVoiceCounter++;
+			resonatorVoiceCounter %= 4;
 		}
 	}
 
 	public void RotateSound(int x) {
 		if (audioON) {
-			OSCHandler.Instance.SendMessageToClient ("MAX", "127.0.0.1", "rotate " + x);
+			//OSCHandler.Instance.SendMessageToClient ("MAX", "127.0.0.1", "rotate " + x);
+			if (x == 3) {
+				currentClip = Random.Range (0, (triangleRotationSounds.Length - 1));
+				rotationPolyphony[rotationVoiceCounter].clip = triangleRotationSounds [currentClip];
+				rotationPolyphony [rotationVoiceCounter].Play ();
+			} else if (x == 4) {
+				currentClip = Random.Range (0, (squareRotationSounds.Length - 1));
+				print ("CC " + currentClip);
+				rotationPolyphony[rotationVoiceCounter].clip = squareRotationSounds [currentClip];
+				rotationPolyphony [rotationVoiceCounter].Play ();			
+			} else if (x == 5) {
+				currentClip = Random.Range (0, (pentagonRotationSounds.Length - 1));
+				rotationPolyphony[rotationVoiceCounter].clip = pentagonRotationSounds [currentClip];
+				rotationPolyphony [rotationVoiceCounter].Play ();
+			}
+
+			rotationPolyphony [rotationVoiceCounter].Play ();
+
+			rotationVoiceCounter++;
+			rotationVoiceCounter %= 4;
 		}
 	}
 
@@ -48,6 +120,7 @@ public class AudioManager : MonoBehaviour {
 	public void GoalBubble() {
 		if (audioON) {
 		//	OSCHandler.Instance.SendMessageToClient ("MAX", "127.0.0.1", "bubble");
+			goalVoice.Play();
 		}
 	}
 
@@ -55,7 +128,25 @@ public class AudioManager : MonoBehaviour {
 	void OnApplicationQuit() {
 		//	message of -1 turns audio off
 		if (audioON) {
-			OSCHandler.Instance.SendMessageToClient ("MAX", "127.0.0.1", "onoff 0");
+			//OSCHandler.Instance.SendMessageToClient ("MAX", "127.0.0.1", "onoff 0");
 		}
 	}
+		
+
+	void InitializeSounds() {
+		triangleSounds = Resources.LoadAll<AudioClip> ("Sounds/Triangle");
+		squareSounds = Resources.LoadAll<AudioClip> ("Sounds/Square");
+		pentagonSounds = Resources.LoadAll<AudioClip> ("Sounds/Pentagon");
+
+		resonatorVoiceCounter = 0;
+
+		triangleRotationSounds = Resources.LoadAll<AudioClip> ("Sounds/RotationTri");
+		squareRotationSounds = Resources.LoadAll<AudioClip> ("Sounds/RotationSqr");
+		pentagonRotationSounds = Resources.LoadAll<AudioClip> ("Sounds/RotationPent");
+
+		print (triangleRotationSounds.Length);
+
+		rotationVoiceCounter = 0;
+	}
+
 }
