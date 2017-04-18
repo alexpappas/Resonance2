6d3cd352edd272740c76425f17d37a0be827b4e4
Ruowen Tan <rt1095@nyu.edu>
2017-04-18 19:36:01 -0400

    Game_over works better..

--- Assets/reset_pop_window.cs
+++ Assets/reset_pop_window.cs
@@ -9,28 +9,34 @@ public class reset_pop_window : MonoBehaviour {
 
 	public GameObject tomove;
 	public static bool out_of_moves = false;
-	public static bool won = false;
+	public static bool finished = false;
 	public Text stage_name;
+	private bool move = false;
 	// Use this for initialization
 	void Start () {
-		Vector3 position = new Vector3 (900, 100, 0);
-		tomove.transform.position = position;
+		//tomove.GetComponent<RectTransform> ().localPosition = new Vector3 (432, -92, 0);
 		tomove.transform.localScale = new Vector3 (1, 1, 1);
 		stage_name.text = SceneManager.GetActiveScene ().name;
 	}
 
-	private float time_after_fail = 0f;
+	private float time_started = 0f;
 	// Update is called once per frame
 	void Update () {
-		if (out_of_moves) {
-			time_after_fail += Time.deltaTime;
+		if (out_of_moves && Input.GetKeyDown (KeyCode.Space) && !finished) {
+			move = true;
 		}
-		if (time_after_fail > 5f && time_after_fail < 7f) {
+
+		if (move && !finished) {
 			Vector3 current_position = tomove.transform.position;
-			float current_x = current_position.x;
 			current_position.x = current_position.x - Time.deltaTime * 82f;
 			tomove.transform.position = current_position;
+			time_started += Time.deltaTime;
 		}
+
+		if (time_started > 2f) {
+			finished = true;
+		}
+
 	}
 
 
