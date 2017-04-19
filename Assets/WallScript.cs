936b8f666b00be7d768c292c029921b6a3f85e68
alexpappas <abp400@nyu.edu>
2017-04-19 09:33:44 -0400

    Updated Player Appearance
    alright… last thing for real
    new player appearance

--- Assets/WallScript.cs
+++ Assets/WallScript.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class WallScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 	void OnTriggerEnter2D(Collider2D coll) {
 		if (coll.gameObject.name.EndsWith ("Pulse")) {
 			AudioManager.instance.SoundBarrier ();
