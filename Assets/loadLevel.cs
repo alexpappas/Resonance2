5f086ce219b160375b8d3e2f19e02258bd96abbd
ac4649 <ac4649@nyu.edu>
2017-04-29 12:14:58 -0400

    Rotating level selector works!
    Now we just need to juice it up similar to the levels (and label which
    levels are which.

---
+++ Assets/loadLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadLevel : MonoBehaviour {
+
+	public string sceneName;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter2D(Collider2D coll) {
+
+		print ("Loading Scene with name " + sceneName);
+		SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+
+	}
+}
